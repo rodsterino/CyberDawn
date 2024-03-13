d32751969f1acff9e835ae1bb3ca2fec9fb0ffd1
rodsterino <157448204+rodsterino@users.noreply.github.com>
2024-03-12 21:47:29 -0400

    Initial Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 moveInput = new Vector3(0f,0f,0f);
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+        
+        //Debug.Log(moveInput);
+        
+        moveInput.Normalize();
+
+        transform.position += moveInput * moveSpeed * Time.deltaTime;
+    }
+}
